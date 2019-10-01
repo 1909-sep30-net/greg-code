406084d2e6319dc6d8c2c2ca6367bbb3dfdad941
Gregory Y Favrot <gpyofavv@gmail.com>
2019-10-01 17:02:40 -0500

    Added DogApplication Class example

---
+++ DogApplication/DogApplication/DogClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogApplication
+{
+    internal class DogClass
+    {
+        //access modifiers
+        /*
+         * public - everyone can see
+         * internal - assembly can see
+         * protected - subclass of class can see (even if subclass is in a different project)
+         * private - class specific
+         * protected internal
+         * private protected
+         * 
+         * private is default, but always specify anyway
+         */
+
+        /*
+         * Class modifiers:
+         * public
+         * internal
+         */
+
+        //fields
+        private string name;
+        private int age;
+
+        //constructor
+        public DogClass(String name, int age)
+        {
+            if(name == null)
+            {
+                throw new ArgumentNullException("name");
+            }
+            this.name = name;
+            Age = age;
+        }
+
+        //properties
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                }
+                age = value;
+            }
+        }
+    }
+}
---
+++ DogApplication/DogApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DogApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var dog = new DogClass("Spot", 2);
+                //dog.name = "Spot";
+                //dog.age = 4;
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch(ArgumentException ex) //Could have used ArgumentNullException, which is in DogClass, but ArgumentException is more general
+            {
+                Console.WriteLine("error, recovering some argument error");
+            }
+        }
+    }
+}
