9887c19ca695a2ab76738a57e64e8a492d8a7c7b
Gregory Y Favrot <gpyofavv@gmail.com>
2019-10-03 18:40:02 -0500

    Add delegates - need to go back and add gitignore

---
+++ Delegates/Delegates/MoviePlayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Delegates
+{
+    class MoviePlayer
+    {
+        //you tell it to play a movie and then an event tells you when it is done
+        public string CurrentMovie { get; set; }
+
+        //type for handling the event
+        public delegate void MovieFinishedHandler(); //defines a delegate TYPE for return = void, param = none
+        //functions with same shape can be SUBCRIBED to this delegate
+
+        //the event member
+        public event MovieFinishedHandler MovieFinished;
+
+        //method to play movie
+        public void PlayMovie()
+        {
+            Console.WriteLine($"Playing {CurrentMovie}");
+
+            Thread.Sleep(3000); //wait 3 seconds
+
+            //trigger(fire) the event - calls all subscribing delegates
+
+            //if there are no subscribers, event is "null"ish and this throws an exception
+            MovieFinished?.Invoke(); //invoke is != null shorthand
+        }
+
+    }
+}
---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movie = "Frozen";
+
+            var moviePlayer = new MoviePlayer { CurrentMovie = movie };
+
+
+            //
+            MoviePlayer.MovieFinishedHandler handler = PrintMovieOver;
+
+            moviePlayer.MovieFinished += handler; //subscribe with +=, unsub with -=
+            //
+
+
+            moviePlayer.PlayMovie();
+
+
+        }
+
+
+        static void PrintMovieOver()
+        {
+            Console.WriteLine("Movie finished");
+        }
+
+        static void PrintMovieOver(string name)
+        {
+            Console.WriteLine($"Movie {name} finished");
+        }
+    }
+}
