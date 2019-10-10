f738405ac674467ef1b470520e01a291e5d05130
Gregory Y Favrot <gpyofavv@gmail.com>
2019-10-10 14:27:23 -0500

    Add frame for Manipulation

---
+++ Employee DB Exercise/Manipulation.sql
@@ -0,0 +1,19 @@
+--add at least 3 records into each table
+
+
+--add employee Tina Smith
+
+
+--add department Marketing
+
+
+--list all employees in Marketing
+
+
+--report total salary of Marketing
+
+
+--report total employees by department
+
+
+--increase salary of Tina Smith to $90000
